153fa61c156ca48ccd7971c5d9e91487f1e66dbe
Sharad Jain <sharad10.jain@gmail.com>
2017-10-14 07:32:46 +0530

    com

--- app.py
+++ app.py
@@ -12,12 +12,18 @@ PAGE_ACCESS_TOKEN = page_token
 
 @app.route('/webhook', methods = ['GET'])
 def verify():
-	if request.args.get("hub.mode") == "suscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == verify_token:
-			return "Verification token mismatch", 403
-		return request.args["hub.challenge"], 200
+	print "Handling Verification"
+	if request.args.get('hub.verify_token', '') == verify_token:
+		print "Verification successful!"
+		return request.args.get('hub.challenge','')
+	else:
+		print "Verification failed!"
+		return 'Error, wrong validation token'
+		#if not request.args.get("hub.verify_token") == verify_token:
+		#	return "Verification token mismatch", 403
+		#return request.args["hub.challenge"], 200
 
-	return verify_token, 200
+	#return verify_token, 200
 
 def logg(mess, meta='log', symbol='#'):
 	#pass
