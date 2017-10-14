b93b0fc6222945c48ef7e828913238d379fd5a45
Sharad Jain <sharad10.jain@gmail.com>
2017-10-14 12:11:21 +0530

    Make it better

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 page_token = "EAAFE2AB12bUBAEdWAU672nnZCqZBmRT2ZAdk8WhWDy7X5YR9rFlVcYtX4uyQsLi9Kv6NyLRfQo3ZAlUkCD2Nl0dFKZBQKEIchUfRWbwoiEFTaPCJs2fMWMnOiEtlW8eN9fkgz8Y14OZAHpTqLN0UfZB2hPJjkIO5jZCWPNzOPh8XTgZDZD"
-verify_token = '    '
+verify_token = ' 2318934571'
 PAGE_ACCESS_TOKEN = page_token
 
 @app.route('/webhook', methods = ['GET'])
